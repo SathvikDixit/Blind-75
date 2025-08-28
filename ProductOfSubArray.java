2bebe0cf1e862d20730727f4f1db37b39fc1fde7
SathvikDixit <sathvikdixit21@gmail.com>
2025-08-28 20:48:59 +0530

    asjf;lkasjfd

--- ProductOfSubArray.java
+++ ProductOfSubArray.java
@@ -30,13 +30,18 @@
 public class ProductOfSubArray {
     public static int prodOfSubarray(int[] arr, int n) {
         int maxi = Integer.MIN_VALUE;
+        int sum = 0;
         for (int i = 0; i < n; i++) {
-            
+            sum += arr[i];
+
+            if (sum > maxi) maxi = sum;
+            if (sum < 0) sum = 0;
+            if (maxi < 0) maxi = 0;
         }
         return maxi;
     }
     public static void main(String[] args) {
-        int[] arr = {1, -2, 3, 4, -5, 6};
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
         int n = arr.length;
         int ans = prodOfSubarray(arr, n);
         System.out.println("The maximum sum of subarray is: " + ans);
