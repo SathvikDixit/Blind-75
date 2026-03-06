5f6700bf3d0dcfdccd2937356388117fa1691483
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-03-06 13:21:57 +0530

    DSA

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,22 +1,23 @@
 public class SecondLargest {
     public static int secondLargest(int[] arr) {
+        int n = arr.length;
         int largest = Integer.MIN_VALUE;
         int secLargest = Integer.MIN_VALUE;
 
-        for (int num : arr) {
-            if (num > largest) {
-                secLargest = largest;
-                largest = num;
-            } 
-            if (num > secLargest && num != largest) {
-                secLargest = num;
+        for (int i = 0; i < n; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > secLargest && arr[i] != largest) {
+                secLargest = arr[i];
             }
         }
-        System.out.println("Largest element is: "+largest);
+        
         return  secLargest;
     }
     public static void main(String[] args) {
-        int[] arr = {4, 2, 9, 10, 5, 1};
+        int[] arr = {4, 2, 9, 8, 5, 1};
         System.out.println("The second largest element is: " + secondLargest(arr));
     }
 }
