ada1dfd6721db870ce34fdc40d6641a38e39cb58
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-03-06 12:45:57 +0530

    found the second smallest element problem

---
+++ SecondSmall.java
@@ -0,0 +1,22 @@
+public class SecondSmall {
+    public static int secondSmallest(int[] arr) {
+        int n = arr.length;
+        int smallest = Integer.MAX_VALUE;
+        int secSmall = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < n; i++) {
+            smallest = Math.min(smallest, arr[i]);
+        }
+        for(int i = 0; i < n; i++) {
+            if (arr[i] < secSmall && arr[i] != smallest){
+                secSmall = arr[i];
+            }
+        }
+
+        return secSmall;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 4, 5};
+        System.out.println("second smalled element is: "+SecondSmall.secondSmallest(arr));
+    }
+}
