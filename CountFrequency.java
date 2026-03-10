eb51ee9c234b3790c7d605910b228b9526709437
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-03-10 12:32:09 +0530

    optimal solution

--- CountFrequency.java
+++ CountFrequency.java
@@ -1,20 +1,26 @@
+import java.util.*;
 public class CountFrequency {
-    public static void countFreq(int[] arr) {
-        boolean[] visited = new boolean[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            if (visited[i])
-                continue;
+    // public static void countFreq(int[] arr) {
+    //     boolean[] visited = new boolean[arr.length];
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (visited[i])
+    //             continue;
 
-            int count = 1;
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[i] == arr[j]) {
-                    visited[j] = true;
-                    count++;
-                }
-            }
-            System.out.println(arr[i]+" "+count);
-        }
-    } //    T.C = O(n*n)
+    //         int count = 1;
+    //         for (int j = i+1; j < arr.length; j++) {
+    //             if(arr[i] == arr[j]) {
+    //                 visited[j] = true;
+    //                 count++;
+    //             }
+    //         }
+    //         System.out.println(arr[i]+" "+count);
+    //     }
+    // } //    T.C = O(n*n)
+
+    public static void countFreq(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+    }
 
     public static void main(String[] args) {
         int[] arr = {4, 8, 2, 9, 1, 4, 2, 0, 0};
