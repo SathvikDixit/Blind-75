219045c33215bb5928b49ab7e7d9b34cd3d782a8
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-03-10 10:26:55 +0530

    solved CountnFrequency problem

--- CountFrequency.java
+++ CountFrequency.java
@@ -1,11 +1,23 @@
 public class CountFrequency {
     public static void countFreq(int[] arr) {
-        
+        boolean[] visited = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i])
+                continue;
+
+            int count = 1;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i]+" "+count);
+        }
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 8, 2, 9, 1, 0};
+        int[] arr = {4, 8, 2, 9, 1, 4, 2, 0, 0};
         countFreq(arr);
     }
-    
 }
