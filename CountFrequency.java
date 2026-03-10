e287098911cc40439c8db8bfaf9ba4b7389dbf9a
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-03-10 10:20:48 +0530

    started countFrequency

---
+++ CountFrequency.java
@@ -0,0 +1,11 @@
+public class CountFrequency {
+    public static void countFreq(int[] arr) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 8, 2, 9, 1, 0};
+        countFreq(arr);
+    }
+    
+}
