bc343c0613407160b4b2d8d130c9c3ae07cf56ae
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-03-12 08:05:05 +0530

    added time and space complixity for the Count Frequency problem

--- CountFrequency.java
+++ CountFrequency.java
@@ -1,38 +1,40 @@
 import java.util.*;
-public class CountFrequency {
-    // public static void countFreq(int[] arr) {
-    //     boolean[] visited = new boolean[arr.length];
-    //     for (int i = 0; i < arr.length; i++) {
-    //         if (visited[i])
-    //             continue;
 
-    //         int count = 1;
-    //         for (int j = i+1; j < arr.length; j++) {
-    //             if(arr[i] == arr[j]) {
-    //                 visited[j] = true;
-    //                 count++;
-    //             }
-    //         }
-    //         System.out.println(arr[i]+" "+count);
-    //     }
-    // } //    T.C = O(n*n)
+public class CountFrequency {
+    /*
+     * public static void countFreq(int[] arr) {
+     * boolean[] visited = new boolean[arr.length];
+     * for (int i = 0; i < arr.length; i++) {
+     * if (visited[i])
+     * continue;
+     * 
+     * int count = 1;
+     * for (int j = i+1; j < arr.length; j++) {
+     * if(arr[i] == arr[j]) {
+     * visited[j] = true;
+     * count++;
+     * }
+     * }
+     * System.out.println(arr[i]+" "+count);
+     * }
+     * } // T.C = O(n*n)
+     */
 
     public static void countFreq(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : arr) {
-            map.put(num, map.getOrDefault(num, 0)+1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey()+" occurs "+entry.getValue()+" times.");
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " occurs " + entry.getValue() + " times.");
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 8, 2, 9, 1, 4, 2, 0, 0};
+        int[] arr = { 4, 8, 2, 9, 1, 4, 2, 0, 0 };
         countFreq(arr);
     }
 }
 
-
-// T.C = O(n)       S.C = O(n`)
+// T.C = O(n) S.C = O(n)
