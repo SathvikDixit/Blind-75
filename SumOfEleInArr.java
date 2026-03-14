85928af64d653b51c326acebcb1ec1d944d9c437
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-03-14 11:10:01 +0530

    Sum of elements in an array with optimal solution

--- SumOfEleInArr.java
+++ SumOfEleInArr.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SumOfEleInArr {
     public static int sumOfEle(int[] arr, int n) {
         int sum = 0;
@@ -6,10 +8,16 @@ public class SumOfEleInArr {
         }
         return sum;
     }
+
+    public static int sumOfEleOptimal(int[] arr, int n) {
+        return Arrays.stream(arr).sum();
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7,8, 9};
         int n = arr.length;
 
         System.out.println(sumOfEle(arr, n));
+
+        System.out.println();
     }
 }
